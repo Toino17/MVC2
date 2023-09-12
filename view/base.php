7b9a0ae858f82ce958b35fe4b9acf460e4e5f319
Toino17 <colonnact@outlook.com>
2023-09-12 16:35:49 +0200

    12/09

--- view/base.php
+++ view/base.php
@@ -5,26 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet">
 </head>
 <body>
     <section class="main">
-        <div class="iphoneDiv">
+        <div class="iphoneImgDiv">
+            <img src="images/pngegg.png" class="iphoneImg" alt="">
+            <div class="iphoneDiv">
             <h1>Contacts</h1>
-            <input class="searchbar" id="searchbar" onkeyup="search_animal()" type="text" name="search" placeholder="Recherche">
             <form method="POST" class="form1">
                 <?php
                 foreach ($tab as $key){
-                echo  $key['nom'] . ' ' . $key['prenom'] . '
-                <button name="updateButton" value="' . $key['id'] . '">Modifier</button>
-                <button name="deleteInput" value="' .$key['id']. '">Supprimer</button>  
-                <br>';
+                echo '<div class="contactDiv">'. $key['nom'] . ' ' . $key['prenom'] . '
+                <button name="updateButton" value="' . $key['id'] . '"><img class="modifierPng" src="images/modifier.png" alt=""></button>
+                <button name="deleteInput" value="' .$key['id']. '"><img class="modifierPng" src="images/croix.png" alt=""></button>  
+                <br></div>';
                 }
                 ?>
                 <br>
-                <button name="addContactButton"><a href="index.php?page=addContact">Créer un nouveau contact</a></button>
+                <button name="addContactButton" class="addContactButton"><a href="index.php?page=addContact"><img class="modifierPng" src="images/bouton-dajout.png" alt=""></a></button>
             </form>
         </div>
-        <img src="images/iphone.png" class="iphoneImg" alt="">
+        </div>
+        
+        
     </section>
 </body>
 </html>
